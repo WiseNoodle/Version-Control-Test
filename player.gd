a31ea355fcb8fb5ed643f76b9d8f386b7aa6ea26
WiseNoodle <wisenoodle21@gmail>
2023-07-09 21:41:22 -0400

    udpc

--- player.gd
+++ player.gd
@@ -9,10 +9,6 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -24,7 +20,6 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
